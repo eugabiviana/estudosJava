6db4f333f01ab1895d0430be165ea3eb5f3c002d
eugabiviana <gabi.viana88@gmail.com>
2022-03-28 14:34:21 -0300

    diferentes tipos de comentários

--- maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,7 +1,20 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Teste de <b>javadoc</b>
+ * Esse tipo de comentário serve para documentar libs. É editado com estilo html e fica antes do nome da classe!
+ * A formatação é feita pelo lápis na barra lateral esquerda.
+ */
 public class OlaDevDojo {
     public static void main(String[] args){
         System.out.println("KA-ME-HA-ME-HA");
     }
+//    comentário de uma linha (ctrl + /)
+    /*
+    comentário de
+    várias
+    linhas.
+    Pode ser com e sem * em cada linha.
+     */
+
 }

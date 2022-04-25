953e1aeef6becb45afad0089ce102fb9a13bde01
eugabiviana <gabi.viana88@gmail.com>
2022-04-25 13:38:13 -0300

    Aula 07 pt 03: Arrays - .length

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -12,5 +12,19 @@ public class Aula07Arrays02 {
         System.out.println(idades[0]);
         System.out.println(idades[1]);
         System.out.println(idades[2]);
+
+        String[] nomes = new String[4];
+        nomes[0] = "Naruto";
+        nomes[1] = "Sasuke";
+        nomes[2] = "Jiraya";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+//        nomes = new String[5];
+
     }
 }
+
+//.length "atualiza o tamanho do array no código" quando alterado na inicialização;
+//também posso alterar o tamanho após o print. A partir desse ponto, o programa utilizará o novo dado.

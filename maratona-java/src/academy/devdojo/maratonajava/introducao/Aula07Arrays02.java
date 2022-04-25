92281fc4ec85a3831a2daefbb528c809abe8e5b0
eugabiviana <gabi.viana88@gmail.com>
2022-04-25 13:08:03 -0300

    Aula 07: Arrays pts 01 e 02 - inicializando e tipo primitivos

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+//        valores padrões de tipos primitivos quando não inicializados: byte, short, int, long, flot e double = 0
+//        char '\n0000' ' '
+//        boolean = false
+//        String = null
+
+        int[] idades = new int[3];
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}

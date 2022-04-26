af35edea424c6be1ff2e06deb610de9ebc317970
eugabiviana <gabi.viana88@gmail.com>
2022-04-26 16:29:20 -0300

    Aula 08: outra forma de inicializar arrays multidimensionais - pt 3.

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -15,13 +15,15 @@ public class Aula08ArraysMultidimensionais01 {
         dias[1][2] = 30;
 
         for (int i = 0; i < dias.length; i++) {
+            System.out.println(" | ");
             for (int j = 0; j < dias[i].length; j++) {
-                System.out.println(dias[i][j]);
+                System.out.print(dias[i][j]);
+                System.out.print(" | ");
             }
         }
 
 //        Usando for each:
-        System.out.println("====================================");
+        System.out.println("\n====================================");
 
         for (int[] arrBase: dias) {
             for (int num : arrBase){

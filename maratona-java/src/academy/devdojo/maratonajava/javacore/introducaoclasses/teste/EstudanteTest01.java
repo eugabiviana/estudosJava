71c48e9963a6af07598062b85f7a2f4a6fc5884f
eugabiviana <gabi.viana88@gmail.com>
2022-05-24 17:24:22 -0300

    Orientacao a objetos - introducao a classes pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/teste/EstudanteTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Dani";
+        estudante.idade = 32;
+        estudante.sexo = 'F';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante); // imprime o endereço de onde o objeto está salvo!
+    }
+}

3fc5b93c7e1d03dffb3c089785d51dee0bd6a9b4
eugabiviana <gabi.viana88@gmail.com>
2022-05-31 16:53:31 -0300

    Orientacao a Objetos - metodos pt 03 - return pt 01

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,5 +12,15 @@ public class Calculadora {
     public void multiplicaDoisNumeros(int num1, float num2){
         System.out.println(num1 * num2);
     }
+
+    public double divideDoisNumeros(double num1, double num2){
+        if (num2 == 0){
+            return 0;
+        } else if(num1 > num2) {
+            return num1 / num2;
+        } else{
+            return num2 / num1;
+        }
+    }
 }
 //Posso colocar dois tipos de números diferentes no parâmetro, desde que isso esteja especificado na criação do método.
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.teste;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(10, 20);
+        System.out.println(result);
+    }
+}
